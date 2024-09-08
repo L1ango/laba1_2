d9f7d68c02228303fafb8688c8188b94ce004f73
L1ango <lyangoegor_2005@mail.ru>
2024-09-08 18:51:23 +0300

    add_change3

---
+++ src/AbstractFurniture.java
@@ -0,0 +1,34 @@
+abstract class AbstractFurniture implements Furniture, Object {
+    protected String manufacturer;
+    protected double price;
+    protected String material;
+
+    public AbstractFurniture(String manufacturer, double price, String material) {
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.material = material;
+    }
+
+    @Override
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getMaterial() {
+        return material;
+    }
+
+    @Override
+    public String info() {
+        return "Manufacturer: " + manufacturer + ", Price: " + price + ", Material: " + material;
+    }
+
+    @Override
+    public abstract void print();
+}
